a724c01736b75cf1936408de6e9a416d3d700f89
Agneta <agnetagsu@yahoo.es>
2021-05-22 22:19:59 +0200

    Agregar archivos de proyecto.

---
+++ CalculadoraInversa/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace CalculadoraInversa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int X,Y;
+            char signo;
+
+            Console.WriteLine("Introduzca el primer operando:");
+            X = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el segundo operando:");
+            Y = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Introduzca el signo aritmético:");
+            signo = Convert.ToChar(Console.ReadLine());
+
+            switch(signo)
+            {
+                case '+':
+
+                    Console.WriteLine("El resultado {0}+{1} = {2}", X, Y, X + Y);
+                    break;
+
+                case '-':
+
+                    Console.WriteLine("El resultado {0}-{1} = {2}", X, Y, X - Y);
+                    break;
+
+                case '*':
+
+                    Console.WriteLine("El resultado {0}*{1} = {2}", X, Y, X * Y);
+                    break;
+
+                case '/':
+
+                    Console.WriteLine("El resultado {0}/{1} = {2}", X, Y,(double) X / Y);
+                    break;
+
+                case '^':
+
+                    Console.WriteLine("El resultado {0}^{1} = {2}", X, Y, Math.Pow(X,Y));
+                    break;
+
+                case '%':
+
+                    Console.WriteLine("El resultado {0}%{1} = {2}", X, Y, X % Y);
+                    break;
+
+                default:
+
+                    Console.WriteLine("Signo aritmético incorrecto");
+                    break;
+
+            }
+        }
+    }
+}
